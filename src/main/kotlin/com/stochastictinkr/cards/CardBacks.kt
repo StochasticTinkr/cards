de1a64212e1ed8b4dcbb195856dcdf151ef1aeac
Daniel Pitts <git@stochastictinker.com>
2025-09-02 06:38:20 -0700

    Enable `FISH` card back in `CardBacks` enum and remove unused SVG flow elements from `fish.svg`.

--- src/main/kotlin/com/stochastictinkr/cards/CardBacks.kt
+++ src/main/kotlin/com/stochastictinkr/cards/CardBacks.kt
@@ -1,6 +1,6 @@
 package com.stochastictinkr.cards
 
-enum class CardBacks(val filename: String, val displayName:String) {
+enum class CardBacks(val filename: String, val displayName: String) {
     ABSTRACT("abstract", "Abstract"),
     CLOUDS("abstract_clouds", "Abstract Clouds"),
     HIGHWAY("abstract_scene", "Abstract Scene"),
@@ -9,7 +9,7 @@ enum class CardBacks(val filename: String, val displayName:String) {
     BLUE2("blue2", "Blue2"),
     CARS("cars", "Cars"),
     CASTLE("castle", "Castle"),
-//    FISH("fish", "Fish"),
+    FISH("fish", "Fish"),
     FROG("frog", "Frog"),
     RED("red", "Red"),
     RED2("red2", "Red2"),
